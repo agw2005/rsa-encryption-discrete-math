e9236a0de50cac719f63cdd5b1892e44aaa504c7
Danial Al Ghazali Walangadi <danial.agw.2005@gmail.com>
2024-09-30 15:48:27 +0700

    improved the arrow png

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,18 @@ function App() {
       <h1 className="text-4xl text-center p-16">
         Encryption with RSA algorithm
       </h1>
-      <div className="flex justify-evenly">
-        <form className="" method="post" onSubmit={handleSubmit}>
+
+      <div className="border-2 border-black border-solid flex justify-center">
+        <form onSubmit={handleSubmit}>
           <textarea
-            className="p-4 border-2 border-black border-solid min-w-96 min-h-96 resize-none"
+            className="p-4 border-2 border-black border-solid min-w-96 min-h-96 max-w-96 max-h-96 resize-none"
             placeholder="Teks yang akan di-enkripsi"
             value={plainText}
             onChange={(clickEvent) => setPlainText(clickEvent.target.value)}
           ></textarea>
-          <br />{" "}
+
+          <br />
+
           <input
             className="border-2 border-black border-solid p-4 bg-gray-300 hover:bg-slate-500 hover:text-white"
             type="submit"
@@ -30,10 +33,16 @@ function App() {
           />
         </form>
 
-        <img className="max-w-64" src="\right-arrow.svg" alt="arrow" />
+        <div className="flex justify-evenly px-4">
+          <img
+            className="border-2 border-black border-solid max-w-64 min-w-64"
+            src="\RSA-arrow.png"
+            alt="arrow"
+          />
+        </div>
 
         <textarea
-          className="p-4 border-2 border-black border-solid min-w-96 min-h-96 resize-none"
+          className="p-4 border-2 border-black border-solid min-w-96 min-h-96 max-w-96 max-h-96 resize-none"
           placeholder="Teks yang telah di-enkripsi"
           value={encryptedText}
           readOnly
